7f89eee4070dec5985ed00357787da4b20b9fefe
Yisak-E <yisakmetaferiya1@gmail.com>
2025-10-05 06:34:56 +0400

    simple update

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -77,7 +77,7 @@ export default function HomePage() {
             navigator: navigateTo
         },
         {
-            title: 'Todo And Done',
+            title: 'Todo',
             image: 'https://img.freepik.com/free-vector/todo-list-concept-illustration_114360-7860.jpg?w=740&t=st=1696117263~exp=1696117863~hmac=3b4c5d6e7f8g9h0i1j2k3l4m5n6o7p8q9r0s1t2u3v4w5x6y7z8',
             description: 'Manage your tasks efficiently with our Todo And Done feature',
             navigateTo: '/TodoAndDone',
